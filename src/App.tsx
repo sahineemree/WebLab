1a8334a95bef888b11ecf289a4ca0be0f6e9f4ca
Emre Şahin <sahineemree@gmail.com>
2026-02-24 15:26:27 +0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB - 1</h2>
+
+      <hr style={{ width: "300px", margin: "20px auto" }} />
+
+      <p><strong>Ad Soyad:</strong> Emre Şahin</p>
+      <p><strong>Ogrenci No:</strong> 235541015</p>
+
+      <p style={{ marginTop: "20px" }}>
+        Bu proje Vite + React + TypeScript kullanilarak olusturulmustur.
+      </p>
+    </div>
+  )
+}
+
+export default App
